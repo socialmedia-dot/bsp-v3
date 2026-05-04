9e722d9aaebe3219e625cbffeff9828511d02b18
Ubuntu <ubuntu@localhost.localdomain>
2026-05-04 09:54:51 +0800

    fix: replace broken dist symlink with actual build output
    - Remove broken dist symlink pointing to local absolute path
    - Add generated static build output to dist/
    - Includes new /BSP/users/consultant route
    - Updates all sidebar links from agent -> consultant

---
+++ dist/_nuxt/bsp.a113c0ba.js
@@ -0,0 +1,1 @@
+import{P as a}from"./entry.7266b92e.js";const o={__name:"bsp",setup(e){return a("/BSP/dashboard",{redirectCode:301}),()=>{}}};export{o as default};
---
+++ dist/_nuxt/logo-bsp.84a8092f.js
@@ -0,0 +1,1 @@
+import"./entry.7266b92e.js";const o=""+globalThis.__publicAssetsURL("img/logo-bsp.jpg");export{o as _};
